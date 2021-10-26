83e40b2b8757921f6adae81c155e5085f1b790fa
Marob-nod <robin.marien@hotmail.fr>
2021-10-26 11:02:49 +0200

    Working on Portfolio style, open infos

--- src/Components/Portfolio/Project/index.tsx
+++ src/Components/Portfolio/Project/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
+import { FC, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconPrefix, IconName, IconProp } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons';
+
 
 
 interface projectProps {
@@ -15,16 +15,30 @@ interface projectProps {
 
 const Project:FC<projectProps> = ({ name, languages, source, github, info, picture }) => {
 
+    const [showInfos, setShowInfos] = useState(false);
+
+    const handleInfo = () => {
+        setShowInfos(!showInfos)
+    }
     return (
         <div className="project">
             <h3>{name}</h3>
-            <img src={picture} alt="photo-projet" />
+            <img src={picture} alt="photo-projet" onClick={handleInfo}/>
             <h4>{languages}</h4>
             <div className="infos">
-                <span className="infos">{info}
-                    <FontAwesomeIcon icon={["fas", "plus-circle"]}/>
+                <span className="" onClick={handleInfo}>
+                    <img src="/media/button-infos.png" alt="" />
                 </span>
             </div>
+            {showInfos && 
+            <div className="show-infos">
+                <div className="infos-content">
+                    <div className="head">
+                        <h2>{name}</h2>
+                    </div>
+                    <div className="button" onClick={handleInfo}>Retourner sur la page</div>
+                </div>
+            </div>}
         </div>
     );
 };
