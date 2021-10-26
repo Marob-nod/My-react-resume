1689e9c22c1818e453683ef19586785d2198438d
Marob-nod <robin.marien@hotmail.fr>
2021-10-26 12:14:07 +0200

    Working on the contact page

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,15 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Sidebar from '../../Components/Sidebar';
 import './styles.scss';
+import linkedinLogo from '../../assets/linkedin.png'
+import githubLogo from '../../assets/github.png'
+
 
 const Contact = () => {
   return (
     <div className="contact">
-      
       <Sidebar />
-      <div className="contact__content">
-      Voici le contenu de la page contacts
-      </div>
+      <div className="contactContent">
+        <div>
+        <div className="header"></div>
+          <div className="contactBox">
+            <h1>Contactez moi</h1>
+            <ul>
+              <li>
+                <div>Paris</div>
+              </li>
+              <li>
+                <div>robin.marien@hotmail.fr</div>
+              </li>
+              <li>
+                <div className='phoneNumber'>06 48 70 67 28</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="socialNetwork">
+          <ul>
+            <a href="https://www.linkedin.com/in/robin-marien-64a571135/" target="_blank" rel="noopener noreferrer">
+              <h4>LinkedIn</h4>
+              <img src={linkedinLogo} alt="" />
+            </a>
+            <a href="https://github.com/Marob-nod" target="_blank" rel="noopener noreferrer">
+              <h4>GitHub</h4>
+              <img src={githubLogo} alt="" />
+            </a>
+          </ul>
+          </div>
+        </div>
     </div>
   );
 };
