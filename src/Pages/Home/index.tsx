5c8047be1d5c0f677370553fca65827041e0e2e8
Marob-nod <robin.marien@hotmail.fr>
2021-11-17 14:54:49 +0100

    update infos

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -12,9 +12,9 @@ const Home = () => {
           <h2>Développeur Frontend</h2>
           <div className="home__content__text__description"></div>
             <p>🚀 Récemment diplômé de mon école d'ingénieur des Mines de Douai et fort d'une première experience professionnelle, je suis à présent spécialisé dans le domaine du développement Web. 
-              <br></br><br></br>Grâce à une formation de qualité de l'Ecole O'Clock ainsi qu'un apprentissage autonome, j'ai développé de nombreuses compétences dans le domaine du frontend notamment. <br></br><br></br>Ma technonolgie de prédilection: le merveilleux framework React.JS !</p> <p>
+              <br></br><br></br>Grâce à une formation de qualité de l'Ecole O'Clock ainsi qu'un apprentissage autonome, j'ai développé de nombreuses compétences dans le domaine du frontend notamment. <br></br><br></br>Ma technologie préférée: le framework React.JS !</p> <p>
 
-              💡 Après avoir participé à plusieurs projets "open-source" collectifs et individuels, je suis à la recherche d'un challenge professionnel pour mettre en pratique mon savoir-faire tout en développant de nouvelles compétences. 
+              💡 Après avoir participé à plusieurs projets "open-source" collectifs et individuels, je suis à la recherche d'un challenge professionnel mettant mon savoir-faire en pratique tout en développant de nouvelles compétences. 
             </p>
         </div>
       </div>
